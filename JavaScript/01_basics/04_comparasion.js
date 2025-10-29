0182b205a6bf1a1a98f9e779b02eaf27bf273ede
MohdNizamuddin49 <githubnizam49@gmail.com>
2025-10-29 13:14:49 +0530

    adding comprision

---
+++ JavaScript/01_basics/04_comparasion.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1); do comparison with sam data type or else it make problem
+// console.log("02" > 1);
+
+// console.log(null > 0); //false
+// console.log(null == 0);//false
+// console.log(null >= 0);//true because the reason is that an equality check == and comparison > < >= <= works differently
+                         // compariion convert null to a number ,treating it as 0 
+                         // thats why in null >= 0 is true and null > 0 is false
+
+
+// console.log(undefined == 0);// false
+// console.log(undefined > 0);// false
+// console.log(undefined < 0);// false
+
+// === strict check 
+
+console.log("2" === 2);
+
+
